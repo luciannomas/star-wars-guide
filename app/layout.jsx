cf514b3c0ddabccf237f10cf17fb8267bab85d3d
luciannomas <luciano.mastran@gmail.com>
2024-12-28 05:37:50 -0300

    change layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,22 +1,20 @@
-import type { Metadata } from "next";
+
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import NextTopLoader from "nextjs-toploader";
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import { ClerkProvider } from "@clerk/nextjs";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Admin TarreCars",
-  description: "web cars",
-};
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+
+export default function RootLayout({ children }) {
   return (
+    
     <ClerkProvider>
       <html lang="en">
         <body className={outfit.className}>
-          <NextTopLoader color="#000" />
+          <NextTopLoader />
           {children}
           {/* <Toaster /> */}
         </body>
