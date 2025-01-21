d743c5d1d735d9d0ad9630eeecd28ee96759cb61
luciannomas <luciano.mastran@gmail.com>
2025-01-21 01:18:36 -0300

    added listCard

--- app/(routes)/(dashboard)/dashboard/page.tsx
+++ app/(routes)/(dashboard)/dashboard/page.tsx
@@ -1,5 +1,8 @@
 import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { db } from "@/lib/db";
+import { ListCars } from "./admin/cars-manager/components/ListCars";
+
 
 export default async function DashboardPage() {
   const { userId } = await auth();
@@ -8,21 +11,21 @@ export default async function DashboardPage() {
     return redirect("/");
   }
 
-  /* const cars = await db.car.findMany({
+  const cars = await db.car.findMany({
     where: {
       isPublish: true,
     },
     orderBy: {
       createdAt: "desc",
     },
-  }); */
+  });
 
   return (
     <div>
       <div className="flex justify-between">
         <h2 className="text-2xl font-bold">List of cars</h2>
       </div>
-      componete cars
+      <ListCars cars={cars} />
     </div>
   );
 }
