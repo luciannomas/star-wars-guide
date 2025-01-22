58109004138a5198835db97cd2bfc338ad43560d
luciannomas <luciano.mastran@gmail.com>
2025-01-22 19:01:57 -0300

    change listcars for filtercars

--- app/(routes)/(dashboard)/dashboard/page.tsx
+++ app/(routes)/(dashboard)/dashboard/page.tsx
@@ -2,6 +2,7 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { db } from "@/lib/db";
 import { ListCars } from "./components/ListCars";
+import { FiltersAndListCars } from "../../(home)/cars/components/FiltersAndListCars";
 
 export default async function DashboardPage() {
   const { userId } = await auth();
@@ -24,7 +25,8 @@ export default async function DashboardPage() {
       <div className="flex justify-between">
         <h2 className="text-2xl font-bold">List of cars</h2>
       </div>
-      <ListCars cars={cars} />
+      {/* <ListCars cars={cars} /> */} {/* lista de autos a reservar sin paginacion */}
+      <FiltersAndListCars cars={cars} /> {/* Con filtro pero sin paginacion */}
     </div>
   );
 }
