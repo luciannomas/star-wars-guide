8ad533b122a57d3bf68f7eb54215b8080ae6592b
luciannomas <luciano.mastran@gmail.com>
2025-02-04 20:03:22 -0300

    added film component mock

--- app/(routes)/(home)/page.tsx
+++ app/(routes)/(home)/page.tsx
@@ -1,10 +1,10 @@
 import { Navbar } from "@/components/Shared/Navbar";
-import { FiltersAndListCars } from "./cars/components/FiltersAndListCars";
 import { db } from "@/lib/db";
+import { FiltersAndListCars } from "./cars/components/FiltersAndListCars";
+import Film from "./films/components/Film/Film";
+
 
 export default async function Home() {
-
-  
   const cars = await db.bobecars.findMany({
     where: {
       isPublish: true,
@@ -14,15 +14,21 @@ export default async function Home() {
     },
   });
 
+  const mockFilm = {
+    title: "A New Hope",
+    episode_id: 4,
+  };
+
   return (
     <div className="container mx-auto px-4">
       <Navbar />
       <div className="my-8 flex justify-center">
         <div className="w-full max-w-5xl">
-          <FiltersAndListCars cars={cars} />
+          {/* <FiltersAndListCars cars={cars} /> */}
+          {/* <ListFilms films={mockFilms} /> */}
+          <Film  />
         </div>
       </div>
     </div>
   );
-
 }
