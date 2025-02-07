8126ef51f4a0b41639c6396f8e289c31b58d8cb6
luciannomas <luciano.mastran@gmail.com>
2025-02-07 01:01:09 -0300

    fix title size

--- components/Shared/Navbar/Navbar.tsx
+++ components/Shared/Navbar/Navbar.tsx
@@ -8,11 +8,11 @@ export function Navbar() {
       <div className="flex flex-col lg:flex-row justify-between items-center">
         <Link href="/" className="flex items-center justify-start gap-x-1 mb-4 lg:mb-0">
           <Image src="/star_wars.svg" alt="Star Wars" width={50} height={50} />
-          <span className="text-2xl font-bold text-border-gold">Star Wars guide</span>
+          <span className="text-3xl font-bold text-border-gold">Star Wars guide</span> {/* Increased font size */}
         </Link>
 
         <div className="flex items-center justify-center gap-x-2 lg:gap-x-4">
-          <Link href="/" className="text-lg hidden lg:block">Movie List</Link> {/* Oculto en móviles */}
+          <Link href="/" className="text-2xl font-semibold text-border-gold hidden lg:block">Movie List</Link> {/* Styled similarly but smaller */}
         </div>
       </div>
     </div>
